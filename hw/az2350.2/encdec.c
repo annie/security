5033204b6ef64eb1d81a45c5f05e717576354c8b
Annie Zhang <azhang496@gmail.com>
2016-11-16 12:44:15 -0500

    quick commit

--- hw/az2350.2/encdec.c
+++ hw/az2350.2/encdec.c
@@ -324,8 +324,8 @@ int main(int argc, char **argv) {
                         free(splitLine[i]);
                     }
                 }
-                free(keyf);
             }
+            free(keyf);
 
             fclose(fp);
         }
